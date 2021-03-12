4db3fae0e087af09e199f97676a8286c090bf462
HuguesBriqueler <hugues.briqueler@gmail.com>
2021-03-13 00:57:03 +0100

    Added game ending (who won)

--- script.js
+++ script.js
@@ -2,13 +2,14 @@ const pierre = document.querySelector(".pierre");
 const feuille = document.querySelector(".feuille");
 const ciseaux =document.querySelector(".ciseaux");
 
-let playerScore, computeurScore, nombreDeManches;
-let uneAutrePartie;
+let playerScore, computeurScore, nombreDeManches, nombreDeTours;
+let uneAutrePartie, whoWonTheGame;
 
 const gameInit = function() {
     playerScore = 0;
     computeurScore = 0;
     uneAutrePartie = "O";
+    nombreDeTours = 0;
     nombreDeManches = 2;
     while(nombreDeManches%2 === 0) {
         nombreDeManches = parseInt(prompt("Combien de manches voulez vous jouer ?  (Entrez un nombre impair)"));
@@ -21,22 +22,37 @@ const gameInit = function() {
 const displayScores = function() {
     document.querySelector(".computerScore").innerHTML = `Moi  : ${computeurScore}`;
     document.querySelector(".playerScore").innerHTML = `Vous  : ${playerScore}`;
+    if (nombreDeTours === nombreDeManches) {
+        if (playerScore > computeurScore) {
+            whoWonTheGame = "You";
+        } else {
+            whoWonTheGame = "I";
+        }
+        uneAutrePartie = prompt(`${whoWonTheGame} Won the game \n Voulez vous faire une autre partie ?`).toUpperCase();
+        if (uneAutrePartie === "O") {
+            gameInit();
+        } /* else {
+            pierre.removeEventListener("click", clickPierre());
+            feuille.removeEventListener("click", clickFeuille());
+            ciseaux.removeEventListener("click", clickCiseaux());
+        } */
+    }
+
 }
 
 const displayWinner = function(winner) {
-    document.querySelector(".scoreTitle").innerHTML = `Scores : ${winner} Win !`;    
-
+    document.querySelector(".scoreTitle").innerHTML = `Scores : ${winner} Win !`;
 }
 
 gameInit();
 
-pierre.addEventListener("click",function(){
+pierre.addEventListener("click",function clickPierre() {
     whatIsClicked(1);
 });
-feuille.addEventListener("click",function(){
+feuille.addEventListener("click",function clickFeuille() {
     whatIsClicked(2);
 });
-ciseaux.addEventListener("click",function(){
+ciseaux.addEventListener("click",function clickCiseaux() {
     whatIsClicked(3);
 });
 
@@ -47,10 +63,12 @@ const whatIsClicked = function(clickStatus) {
         displayWinner("Nobody");
     } else if (computerChoice-1 === clickStatus || (computerChoice===1 && clickStatus===3)){
         computeurScore += 1;
+        nombreDeTours++;
         displayWinner("Computer");
     }
     else {
         playerScore += 1;
+        nombreDeTours++;
         displayWinner("You");
     }
     displayScores();
