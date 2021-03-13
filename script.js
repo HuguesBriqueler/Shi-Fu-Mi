ef05fe81c215c91e95b450bcffdb6528e6d6ae58
HuguesBriqueler <hugues.briqueler@gmail.com>
2021-03-13 03:37:38 +0100

    A bit more enhancement

--- script.js
+++ script.js
@@ -16,6 +16,7 @@ const gameInit = function() {
     }
     document.querySelector(".playerAction").style.backgroundImage = getPicture(4);
     document.querySelector(".computerAction").style.backgroundImage = getPicture(4);
+    document.querySelector(".scoreTitle").innerHTML = `Scores : Let's play...`;
     displayScores();
 }
 
@@ -44,15 +45,7 @@ const displayWinner = function(winner) {
     document.querySelector(".scoreTitle").innerHTML = `Scores : ${winner} Win !`;
 }
 
-pierre.addEventListener("click",function clickPierre() {
-    whatIsClicked(1);
-});
-feuille.addEventListener("click",function clickFeuille() {
-    whatIsClicked(2);
-});
-ciseaux.addEventListener("click",function clickCiseaux() {
-    whatIsClicked(3);
-});
+
 
 const whatIsClicked = function(clickStatus) {
     let computerChoice = Math.floor(Math.random() * 3 + 1);
@@ -91,5 +84,15 @@ const getPicture = function(pictureNumber) {
             break;
     }
 }
+
+pierre.addEventListener("click",function clickPierre() {
+    whatIsClicked(1);
+});
+feuille.addEventListener("click",function clickFeuille() {
+    whatIsClicked(2);
+});
+ciseaux.addEventListener("click",function clickCiseaux() {
+    whatIsClicked(3);
+});
 
 gameInit();
